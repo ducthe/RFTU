8feb0ca45f4c77da36753e75892e570e053c3aab
Peter <vdthe@humaxdigital.com>
2016-09-01 13:03:45 +0700

    1. Author: Peter <vdthe@humaxdigital.com> 2. [RFTU] 3. Ticket 4. Changes: 			create some init object in rftu.h

---
+++ include/rftu.h
@@ -0,0 +1,19 @@
+/*
+* Filename: rftu.h
+* Author: 
+* Date: 01-Sep-2016
+* 
+*
+
+
+*/
+/*************************************************************************/
+/* Include file */
+
+/*Defined*/
+
+
+/*Global variables*/
+
+
+/*Globale functions*/
---
+++ source/rftu.c
@@ -0,0 +1,17 @@
+/*
+* Filename: main.c
+* Author: Peter + Richard
+* Date: 01-Sep-2016
+* 
+*
+
+
+*/
+/*************************************************************************/
+/* Include file */
+
+/*Defined*/
+
+
+/*Global variables*/
+/*Globale functions*/
