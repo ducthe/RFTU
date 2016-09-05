3dc198d1fd8642c04589466fb413f193c2ea6962
Peter <vdthe@humaxdigital.com>
2016-09-05 09:44:14 +0700

    1. Author: Peter <vdthe@humaxdigital.com> 2. [RFTU] 3. Ticket 4. Changes: 			+ Change rftu.h 			+ Add rftu.c follow to Henry

--- include/rftu.h
+++ include/rftu.h
@@ -1,10 +1,23 @@
+/*
+* Filename: rftu.h
+* Author: Henry
+* Contributor: Peter + Richard + Kevin + Issac
+* Date: 01-Sep-2016
+* 
+*
+
+
+*/
+/*************************************************************************/
 #ifndef __RFTU_H__
 #define __RFTU_H__
 
-/*-------------------*/
-/*-----LIBRARIES-----*/
-/*-------------------*/
+///////////////
+// Libraries //
+///////////////
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
@@ -14,10 +27,12 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 
-/*-------------------*/
-/*------MACROS-------*/
-/*-------------------*/
-// RFTU Commands IDs
+////////////
+// MACROS //
+////////////
+//#define DROPPER
+
+// RFTU COMMANDS
 #define RFTU_CMD_NULL      0x00
 #define RFTU_CMD_INIT      0x01
 #define RFTU_CMD_DATA      0x02
@@ -27,21 +42,34 @@
 #define RFTU_CMD_ERROR 	   0x06
 #define RFTU_CMD_COMPLETED 0x07
 
-// RFTU Default Settings
+// RFTU RETURNED VALUES
+#define RFTU_RET_OK 		0
+#define RFTU_RET_ERROR     -1
+
+// RFTU CONSTANTS
+#define YES 1
+#define NO  0
+
+// RFTU RUNNING MODES
+#define RFTU_MODE_NON	   -1
+#define RFTU_MODE_SENDER 	0
+#define RFTU_MODE_RECEIVER 	1
+
+// RFTU SETTINGS
 #define RFTU_SEGMENT_SIZE   1024
 #define RFTU_PORT 			8888
 #define RFTU_TIMEOUT		3
 #define RFTU_MAX_RETRY		10
 #define RFTU_WINDOW_SIZE	8
 
-// RFTU Control Flags
-extern unsigned char flagServer;
-extern unsigned char flagVerbose;
-extern unsigned char flagFile;
-extern unsigned char flagIP;
+// RFTU CONTROL FLAGS
+extern unsigned char flag_server;
+extern unsigned char flag_verbose;
+extern unsigned char flag_file_ok;
+extern unsigned char flag_ip_ok;
 
-// RFTU Data Packet
-struct rftuDataPkt {
+// RFTU DATA PACKAGE
+struct rftu_package_data_t {
 	unsigned char 	cmd;						/* if cmd = DATA or INIT, size and data is available */
 	unsigned char 	id;
 	unsigned int  	seq;
@@ -49,51 +77,53 @@ struct rftuDataPkt {
 	unsigned char 	data[RFTU_SEGMENT_SIZE];
 };
 
-// RFTU Command Packet
-struct rftuCmdPkt {
+// RFTU COMMAND PACKAGE
+struct rftu_package_cmd_t {
 	unsigned char 	cmd;
 	unsigned char 	id;
 	unsigned int  	seq;
 	unsigned short 	size;
 };
 
-// RFTU File Info
-struct fileInfo {
-	char 				fileName[256];
-	unsigned long int  	fileSize;
+// RFTU FILE INFO
+struct file_info_t {
+	char 				filename[256];
+	unsigned long int  	filesize;
 };
 
-// RFTU Sending Windows
-struct windows {
+// RFTU SENDING WINDOWS
+struct windows_t {
 	unsigned char sent;
 	unsigned char ack;
-	struct rftuDataPkt pkt;
+	struct rftu_package_data_t package;
 };
 
-// RFTU Global Variables
-extern char 				rftuFilename[256];
-extern unsigned long int  	rftuFilesize;
-extern char 				rftuIP[20];
-extern unsigned short 		rftuID;
+// RFTU Globla Variables
+// these are to keep track file and connection in a  transaction
+extern char 				rftu_filename[256];
+extern unsigned long int  	rftu_filesize;
+extern char 				rftu_ip[20];
+extern unsigned short 		rftu_id;
 
-/*-----------------------------*/
-/*-----FUNCTION PROTOTYPES-----*/
-/*-----------------------------*/
+/////////////////////////
+// FUNCTIONS PROTOTYPE //
+/////////////////////////
+
 // Global functions
-void 			dispHelp(void);
-unsigned char 	checkIP(char *);
-unsigned char 	checkFileExist(char *);
-void 			socketError(int , char *);
+void 			disp_help(void);
+unsigned char 	check_ip(char *ip);
+unsigned char 	check_file_exist(char *path);
+void 			socket_error(int socket_fd, char *message);
 
-// Sender functions (rftuSender.c)
-unsigned char 		rftuSender(void);
-char*				getFileName(char *);
-unsigned long int  	getFileSize(char *);
-void 				addPkt(struct windows *, unsigned char , int , unsigned int *);
-void 				removePkt(struct windows *, unsigned char , unsigned int );
-void 				sendPkt(struct windows *, unsigned char , int , struct sockaddr_in *, unsigned char );
+// Sender's functions - in rftu_sender.c
+unsigned char 		rftu_sender(void);
+char*				get_filename(char *path);
+unsigned long int  	get_filesize(char *path);
+void 				add_packages(struct windows_t *windows, unsigned char N, int file_fd, unsigned int *seq);
+void 				remove_package(struct windows_t *windows, unsigned char N, unsigned int seq);
+void 				send_packages(struct windows_t *windows, unsigned char N, int socket_fd, struct sockaddr_in *si_other, unsigned char all);
 
-// Receiver functions (rftuReceiver.c)
-unsigned char 	rftuReceiver(void);
+// Receiver's functions - in rftu_receiver.c
+unsigned char 	rftu_receiver(void);
 
 #endif
